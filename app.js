f5db8233a111effd07270f2807de95518d1772d0
Cesar Cisneros <cecisner@ucsd.edu>
2014-04-16 11:25:55 -0700

    login facebook, not using .env

--- app.js
+++ app.js
@@ -3,16 +3,47 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
+var graph = require('fbgraph'); //fb api
 var app = express();
 var dotenv = require('dotenv');
 dotenv.load();
 
 //route files to load
 var index = require('./routes/index');
+var loggedin = require('./routes/loggedin');
+
+
+// this should really be in a config file!
+
+var conf = {
+    client_id:      '231469240376504'
+  , client_secret:  'da9ba9f03fcb8d3bf262e9e9a2a08cb1'
+  , scope:          'email, user_about_me, user_birthday, user_location, publish_stream'
+  , redirect_uri:   'http://localhost:3000/auth/facebook'
+};
 
 //database setup - uncomment to set up your database
 //var mongoose = require('mongoose');
 //mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/DATABASE1);
+
+// Configuration
+/* FB GRAPH CONFIGURATION - no express
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+}); */
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
@@ -23,6 +54,48 @@ app.use(express.bodyParser());
 
 //routes
 app.get('/', index.view);
+app.get('/loggedin', loggedin.view);
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render("index", { title: "click link to connect" });
+});
+
+app.get('/auth/facebook', function(req, res) {
+
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+    var authUrl = graph.getOauthUrl({
+        "client_id":     conf.client_id
+      , "redirect_uri":  conf.redirect_uri
+      , "scope":         conf.scope
+    });
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+    return;
+  }
+
+  // code is set
+  // we'll send that and get the access token
+  graph.authorize({
+      "client_id":      conf.client_id
+    , "redirect_uri":   conf.redirect_uri
+    , "client_secret":  conf.client_secret
+    , "code":           req.query.code
+  }, function (err, facebookRes) {
+    res.redirect('/loggedin');
+  });
+
+
+});
+
+
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
